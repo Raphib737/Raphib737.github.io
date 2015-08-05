ae9ec1176a1fb41a2a315c1d171117146d737b6b
Raphib737 <raph737@bu.edu>
2015-08-05 03:41:15 -0400

    changed settings and updated test environment

--- test.js
+++ test.js
@@ -1,20 +1,30 @@
 $(document).ready(function(){
 	var ctx = $('#myChart').get(0).getContext("2d");
 
-	var data = {
-	    labels: ["Python", "Java", "C"],
-	    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "lightblue",
-            strokeColor: "black",
-            pointColor: "lightBlue",
-            pointStrokeColor: "DarkBlue",
-            pointHighlightFill: "lightBlue",
-            pointHighlightStroke: "lightblue",
-            data: [1,2,3]
-        }
-    	]
-};
+    var data = {
+        labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 90, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 96, 27, 100]
+            }
+        ]
+    };
 	var myRadarChart = new Chart(ctx).Radar(data);
 });
